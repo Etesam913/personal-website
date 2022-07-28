3f201c4711dad92d164c3a79fee0aed4987e952f
Etesam Ansari <etesam1@fb.com>
2022-07-28 16:23:27 -0400

    📝 Added Meta internship to experiences page

--- src/pages/ExperiencePage/Experience.js
+++ src/pages/ExperiencePage/Experience.js
@@ -5,19 +5,35 @@ import ExperienceItem from "./ExperienceItem";
 function Experience() {
   const experienceItems = [
     {
+      Meta: {
+        imgSrc:
+          "https://etesam.nyc3.digitaloceanspaces.com/Personal-Website/meta-logo.png",
+        altText: "Meta Logo",
+        title: "Frontend Engineer Intern",
+        timePeriod: "May 2022 - August 2022",
+        points: [
+          "Worked alongside two other software developers on Mephisto, a data collection platform that allows researchers to easily publish tasks that workers can complete for money.",
+          "I published a npm library with a two React.js components, tips and feedback.",
+          "These components were used in tandem with the Python backend to improve worker-to-worker and worker-to-researcher communication.",
+          "Made other various improvements like adding a search bar to the documentation website and beautifying the command line interface.",
+        ],
+        companyTitle: "Meta",
+        link: "https://www.meta.com/",
+      },
+    },
+    {
       Genesys: {
         imgSrc:
           "https://etesam.nyc3.digitaloceanspaces.com/Personal-Website/genesys-logo.png",
         altText: "Genesys Logo",
-        title: "Software Developer Intern",
-        timePeriod: "June 2021-August 2021",
+        title: "Frontend Engineer Intern",
+        timePeriod: "June 2021 - August 2021",
         companyTitle: "Genesys",
         points: [
           "Worked with two other developers to create a serverless application that automates the uploading of VPAT (Voluntary Product Accessibility Template) files whenever a change is made to the VPAT in the repository.",
           "Created a dashboard which displayed the VPAT files from the database as well as other important information (file name, team name, team contact email, etc...).",
           "Used an AWS application load balancer, lambdas, DynamoDB, s3, and Jenkins to make this possible.",
           "Worked with one other developer to migrate an old AngularJS app to modern Angular 11.",
-          "Used technologies like RxJS to manage state flow",
         ],
         link: "https://www.genesys.com/",
       },
@@ -28,7 +44,7 @@ function Experience() {
           "https://etesam.nyc3.digitaloceanspaces.com/Personal-Website/correlation-one-logo.png",
         altText: "Correlation One Logo",
         title: "Software Developer Intern",
-        timePeriod: "June 2020-Sept 2020",
+        timePeriod: "June 2020 - Sept 2020",
         companyTitle: "Correlation One",
         points: [
           "Worked with two other developers to make substantial UI improvements to the Assess, Connect, and About pages of the main website using React.js.",
@@ -43,7 +59,7 @@ function Experience() {
           "https://etesam.nyc3.digitaloceanspaces.com/Personal-Website/spec-logo.png",
         altText: "Spectator Logo",
         title: "Software Developer",
-        timePeriod: "Sept 2019-Present",
+        timePeriod: "Sept 2019 - Present",
         companyTitle: "Columbia Daily Spectator",
         points: [
           "Worked in a team with 10 other developers to overhaul Columbia’s leading student newspaper’s website with React.js. The website receives 50 million impressions annualy.",
