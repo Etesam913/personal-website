17d96671d0ec456b7cf06eb8b9fe23a08420783b
Etesam913 <etesamansari13@gmail.com>
2023-05-06 00:13:36 -0400

    :construction: Started work on navbar
    Moved away from styled-component because they do not work with ssr

---
+++ src/app/(Navbar)/Navbar.tsx
@@ -0,0 +1,12 @@
+import './navbar.css'
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="navbar-desc">
+        <h2 className='navbar-green'>Etesam Ansari</h2>
+        <h2>Software Developer</h2>
+        <h2>Columbia Grad</h2>
+      </div>
+    </nav>
+  )
+}
