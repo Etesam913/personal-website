7ffdc6f426b6c3ebc7fb90af5b32599091d6a1b4
etesam <etesamansari13@gmail.com>
2023-07-23 13:34:49 -0400

    :globe_with_meridians: Added my social media links to about page
    Removed package-lock.json as yarn is being used

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,24 +4,24 @@ import "./about.css";
 export default function About() {
   return (
     <>
-      <div className='header-row-container'>
+      <div className="header-row-container">
         <Image
           style={{ marginRight: "1rem" }}
           height={48}
           width={48}
-          alt='Icon of Me'
+          alt="Icon of Me"
           priority
-          src='https://etesam.nyc3.cdn.digitaloceanspaces.com/Personal-Website/about-icon.png'
+          src="https://etesam.nyc3.cdn.digitaloceanspaces.com/Personal-Website/about-icon.png"
         />
         <h1>About Me</h1>
       </div>
 
-      <div className='avatar-container'>
+      <div className="avatar-container">
         <Image
           priority
           fill
-          src='https://etesam.nyc3.cdn.digitaloceanspaces.com/Personal-Website/etesam-avatar.png'
-          alt='avatar of Etesam'
+          src="https://etesam.nyc3.cdn.digitaloceanspaces.com/Personal-Website/etesam-avatar.png"
+          alt="avatar of Etesam"
         />
       </div>
       <p>
@@ -33,6 +33,24 @@ export default function About() {
         Bachelors of Science in Computer Science.
       </p>
       <p>When I am not coding, I like to play basketball.</p>
+      <h2>My Links</h2>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/etesam-ansari/">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://www.github.com/Etesam913/">GitHub</a>
+        </li>
+        <li>
+          <a href="https://twitter.com/EtesamAnsari">Twitter</a>
+        </li>
+        <li>
+          <a href="https://www.reddit.com/user/Smogchalk/">Reddit</a>
+        </li>
+        <li>
+          <a href="https://codesandbox.io/u/Etesam913">Codesandbox</a>
+        </li>
+      </ul>
     </>
   );
 }
