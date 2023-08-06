6fa3e2bc6e44772681c8f85aa1b6608428cd978e
etesam <etesamansari13@gmail.com>
2023-08-05 22:35:43 -0400

    :dizzy: Added wave animation for hand emoji

--- src/app/navbar/navbar.tsx
+++ src/app/navbar/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav>
       <div className="navbar-desc">
-        <h2 className="navbar-green">Etesam Ansari</h2>
+        <h2 className="navbar-green">Etesam Ansari <span className="wave">👋</span> </h2>
         <h2>Software Developer</h2>
         <h2>Columbia Grad</h2>
       </div>
